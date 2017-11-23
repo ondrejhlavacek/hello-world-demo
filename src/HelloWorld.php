ffb586e0ee5792a7f3fb4394d45294d86d7f8a53
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-11-23 14:47:06 +0100

    feat: HelloWorld

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -4,5 +4,15 @@ namespace PosledniSobota;
 
 class HelloWorld
 {
+    private $name;
 
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function sayHello()
+    {
+        return 'Hello ' . $this->name;
+    }
 }
